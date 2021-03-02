b5fcf2e2a8fe9db597d4635947b0eafe66dbbcf1
J Scott Rumptz <jscott@rumptz.tech>
2021-03-02 17:05:44 -0600

    first query success

--- db/database.js
+++ db/database.js
@@ -2,28 +2,17 @@
 const mysql = require('mysql2');
  
 // create the connection to database
-const connection = mysql.createConnection({
+const con = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: '11Griffin55!!!',
     database: 'company_db'
 });
 
-// simple query
-connection.query(
-    'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-    function(err, results, fields) {
-        console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
-    }
-);
-   
-// with placeholder
-connection.query(
-    'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-    ['Page', 45],
-    function(err, results) {
-        console.log(results);
-    }
-);
+con.promise().query("SELECT * employees")
+  .then( ([rows,fields]) => {
+    console.log(rows);
+  })
+  .catch(console.log)
+  .then( () => con.end());
